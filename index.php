8b99ba20b251ee0913c267d479a791bc48f5a182
LucasFregonezi <lucasfregonezi@gmail.com>
2021-09-11 22:23:32 -0300

    ajustes na classe usuario

--- index.php
+++ index.php
@@ -2,20 +2,32 @@
 
 require_once("config.php");
 
-//$sql = new Sql();
+//Carrega um usuário
+//$root = new Usuario();
 
-//$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+//$root->loadbyId(3);
 
-//echo json_encode($usuarios);
-
-
-$root = new Usuario();
-
-$root->loadbyId(3);
-
-echo $root;
+//echo $root;
 
 
+//////////////////////////////////
+//Carrega uma lista de usuarios
 
+//$lista = Usuario::getList();
+
+//echo json_encode($lista);
+
+//Carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("uc");
+
+//echo json_encode($search);
+
+
+//Carrega um usuário usando o login e a senha
+
+$usuario = new Usuario();
+$usuario->login("root", "!@#$");
+
+echo $usuario;
 
 ?>
